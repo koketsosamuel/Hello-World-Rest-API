59ace74cb410b0f267cbbf800def044c0c881888
Sam <koklathewebguy@gmail.com>
2019-09-23 22:22:59 +0200

    d

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+app.get("/", function (req, res) {
+  try {
+    res.send("Hello World")
+  } catch(err) {
+    res.json({hi: "hello"})
+  }
 })
 
 app.listen(process.env.PORT || 3000)
